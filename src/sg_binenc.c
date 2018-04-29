4f017506512b1b0f713f59c932a3db09737225f4
threexc <tvgamblin@gmail.com>
2018-04-29 12:38:10 -0400

    Added cleaned-up reading and decoding functions for test purposes

--- src/sg_binenc.c
+++ src/sg_binenc.c
@@ -1,6 +1,12 @@
+/* sg_codec - Tools for encoding and decoding I/Q data for use with the
+E4438C signal generator */
+
 #include <math.h>
 #include <stdio.h>
 
+/* sg_binenc - interleave and encode I/Q data according to E4438C requirements
+(16-bit signed, Big-Endian integers), then place it in an array allocated for
+the complete waveform */
 int sg_binenc(double *i_array, double *q_array, int num_points,
 							short int *waveform_array)
 {
@@ -8,10 +14,11 @@ int sg_binenc(double *i_array, double *q_array, int num_points,
 	double scale;
 	char *pChar;
 	char buf;
-
+	/* Choose a starting default value for min and max amplitudes */
 	double max_amp = i_array[0];
 	double min_amp = i_array[0];
 
+	/* Find the max and minimum values in the data */
 	for (i = 0; i < num_points; i++)
 	{
 		if (max_amp < i_array[i])
@@ -33,9 +40,11 @@ int sg_binenc(double *i_array, double *q_array, int num_points,
 		}
 	}
 
+	/* Get the absolute values of the maximum and minimum values */
 	max_amp = fabs(max_amp);
 	min_amp = fabs(min_amp);
 
+	/* Swap max and min if the maximum's absolute value is smaller */
 	if (min_amp > max_amp)
 	{
 		max_amp = min_amp;
@@ -51,7 +60,7 @@ int sg_binenc(double *i_array, double *q_array, int num_points,
 
 	/* Swap bytes to Big-Endian */
 	pChar = (char *)&waveform_array[0];
-	for( i=0; i<2*num_points; i++ )
+	for( i = 0; i<2*num_points; i++ )
 	{
 		buf = *pChar;
 		*pChar = *(pChar+1);
@@ -62,11 +71,49 @@ int sg_binenc(double *i_array, double *q_array, int num_points,
 	return 0;
 }
 
-int sg_bindec(void *binary_file)
+/* sg_bindec - Decode binary data from 16-bit signed, Big-Endian integers into
+original format (testing purposes only) */
+int sg_bindec(double *i_array, double *q_array, int num_points,
+							short int *waveform_array)
 {
+	/* Swap bytes back to Little-Endian. This block should be the same as the
+	one used to encode to Big-Endian in sg_binenc */
+	pChar = (char *)&waveform_array[0];
+	for( i=0; i<2*num_points; i++ )
+	{
+		buf = *pChar;
+		*pChar = *(pChar+1);
+		*(pChar+1) = buf;
+		pChar+= 2;
+	}
+	/* De-interleave the I and Q data into respective arrays */
+	for (i = 0; i < num_points; i++)
+	{
+		i_array[i] = waveform_array[2*i] / 32767.0;
+	  q_array[i] = waveform_array[2*i+1] / 32767.0;
+	}
+
 	return 0;
 }
 
+/* Read the encoded waveform from a binary file */
+int read_encoded(char *filename, short int *waveform_array, int num_points)
+{
+	FILE *stream = NULL;
+	stream = fopen(filename, "r+b");
+	if (stream == NULL)
+	{
+		perror ("Cannot open file");
+	}
+
+	int num_read = fread((void *)waveform_array, sizeof(short), num_points*2,
+											 stream);
+	fclose(stream);
+
+	return 0;
+}
+
+/* Write an encoded waveform into a binary file */
 int write_encoded(char *filename, short int *waveform_array, int num_points)
 {
 	FILE *stream = NULL;
@@ -76,7 +123,8 @@ int write_encoded(char *filename, short int *waveform_array, int num_points)
 		return 0;
 	}
 
-	int num_written = fwrite((void *)waveform_array, sizeof(short), num_points*2, stream);
+	int num_written = fwrite((void *)waveform_array, sizeof(short), num_points*2,
+													 stream);
 	fclose(stream);
 
 	return 0;
