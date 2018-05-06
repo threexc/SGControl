c357819b54db183f694f1993b0c81249a6b78502
threexc <tvgamblin@gmail.com>
2018-05-06 12:52:07 -0400

    Renamed sg_binenc.h to sg_bincodec.h and added missing function declaration

--- include/sg_bincodec.h
+++ include/sg_bincodec.h
@@ -1,9 +1,12 @@
+
 #ifndef SG_BINENC_H
 #define SG_BINENC_H
 
 int sg_binenc(void *i_data, void *q_data, char *filename);
 
 int write_encoded(char *filename, short int *waveform_array, int num_points);
+
+int read_encoded(char *filename, short int *waveform_array, int num_points);
 
 int sg_bindec(void *binary_file);
 
